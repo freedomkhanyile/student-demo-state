09a3ebc96b2e7955480f39d54a95edc1d05d94d6
Freedom Khanyile <freedom.khanyile1@gmail.com>
2024-10-21 16:36:45 +0200

    feature: add mat-tab on the app component view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('flyInOut', [
+      transition('*=>inmore', [
+        style({ transform: 'translateX(100%)' }), //start in
+        animate(200),
+      ]),
+      transition('*=>inless', [
+        style({ transform: 'translateX(-100%)' }), //start in
+        animate(200),
+      ]),
+      transition('*=>outless', [
+        animate(200, style({ transform: 'translateX(100%)' })), //end in
+      ]),
+      transition('*=>outmore', [
+        animate(200, style({ transform: 'translateX(-100%)' })), //end in
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'student-demo-state';
+  index = 0;
+  indexOld = 0;
+  animationDone = false;
+  constructor(private router: Router) {}
+
+  getAnimation(i: number, indexOld: number, index: number) {
+    const from = i == index ? 'in' : i == indexOld ? 'out' : null;
+    if (from && indexOld != index)
+      return from + (indexOld > index ? 'less' : 'more');
+    return null;
+  }
+  navigate(index: number) {
+    const path = ['/students', '/courses'][index];
+    this.router.navigate([path]);
+  }
 }
--- src/app/shared/material/material-imports.ts
+++ src/app/shared/material/material-imports.ts
@@ -3,10 +3,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+
 export const MATERIAL_MODULES = [
     MatListModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTabsModule
 ]
